9ad2b7db6ef6a1c91852c61449eb5a11ab32f150
trungpdm-shinway <trungpdm@shinway.vn>
2016-05-02 00:26:33 +0700

    add seed FollowTableSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call('CategoryTableSeeder');
         $this->call('TypeTableSeeder');
         $this->call('PostTableSeeder');
+		$this->call('FollowTableSeeder');
     }
 }
